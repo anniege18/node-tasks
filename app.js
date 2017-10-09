4042db946699909b0423273bbc7ba43426839ab9
Anna Garmashevska <anna.garmashevska@gmail.com>
2017-10-10 01:31:41 +0300

    add homework3

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import { name } from './config';
 import * as models from './models';
 import dirWatcher from './dirwatcher';
 import Importer from './importer';
+import start from './utils/streams';
 
-dirWatcher.watch('./data', 2000);
-const importer = new Importer();
-
+// dirWatcher.watch('./data', 2000);
+// const importer = new Importer();
+start();
 console.log(`App name: ${name}`);
 
 new models.User;
