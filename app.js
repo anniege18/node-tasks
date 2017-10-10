f483f9a7f89ddd5e641a6fa22184f16b9dadc937
Anna Garmashevska <anna.garmashevska@gmail.com>
2017-10-10 23:13:14 +0300

    refactor

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ import { name } from './config';
 import * as models from './models';
 // import DirWatcher from './dirwatcher';
 // import Importer from './importer';
-import runUtil from './utils/streams';
+import Streams from './utils/streams';
 
 // const dirWatcher = new DirWatcher();
 // dirWatcher.watch('./data', 2000);
 // const importer = new Importer(dirWatcher);
 // importer.import();
 // importer.importSync();
-
-runUtil();
+new Streams().runUtil();
 
 console.log(`App name: ${name}`);
 
