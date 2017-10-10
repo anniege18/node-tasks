b8cb8c491c6359a5644a97977dbf28de466f14af
Anna Garmashevska <anna.garmashevska@gmail.com>
2017-10-10 23:33:27 +0300

    format

--- dirwatcher.js
+++ dirwatcher.js
@@ -6,40 +6,40 @@ const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 
 class DirWatcher extends EventEmitter {
-  constructor() {
-    super();
-    this.folderFiles = {};
-  }
+    constructor() {
+        super();
+        this.folderFiles = {};
+    }
 
-  watch(folderPath, delay) {
-    setInterval(async() => {
-      try {
-        const stats = await stat(folderPath);
+    watch(folderPath, delay) {
+        setInterval(async() => {
+            try {
+                const stats = await stat(folderPath);
 
-        if (stats.isDirectory()) {
-          const files = await readdir(folderPath);
+                if (stats.isDirectory()) {
+                    const files = await readdir(folderPath);
 
-          files.forEach(async (file) => {
-			const path = `${folderPath}/${file}`;
-            const fileStats = await stat(path);
-            const lastModified = fileStats.mtimeMs;
+                    files.forEach(async (file) => {
+                        const path = `${folderPath}/${file}`;
+                        const fileStats = await stat(path);
+                        const lastModified = fileStats.mtimeMs;
 
-            if (!this.folderFiles[file]) {
-				this.folderFiles[file] = lastModified;
-				this.emit('dirwatcher:changed', path, file);
-				console.log(`${file} was added.`);
-            } else if (this.folderFiles[file] !== lastModified) {
-				this.folderFiles[file] = lastModified;
-  				this.emit('dirwatcher:changed', path, file);
-				console.log(`${file} was modified.`);
+                        if (!this.folderFiles[file]) {
+                            this.folderFiles[file] = lastModified;
+                            this.emit('dirwatcher:changed', path, file);
+                            console.log(`${file} was added.`);
+                        } else if (this.folderFiles[file] !== lastModified) {
+                            this.folderFiles[file] = lastModified;
+                            this.emit('dirwatcher:changed', path, file);
+                            console.log(`${file} was modified.`);
+                        }
+                    });
+                }
+            } catch (err) {
+                console.error('Error:', err);
             }
-          });
-        }
-      } catch (err) {
-        console.error('Error:', err);
-      }
-    }, delay);
-  }
+        }, delay);
+    }
 }
 
 export default DirWatcher;
