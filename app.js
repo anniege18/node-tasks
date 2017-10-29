896f633a9daff7369c7ebcc92e02f6d02f9dd7a5
Anna Garmashevska <anna.garmashevska@gmail.com>
2017-10-29 03:55:35 +0300

    clean up

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 import express from 'express';
 import router from './routes';
-const app = express();
-
 import cookieParser from './middlewares/parse-cookie';
 import queryParser from './middlewares/parse-query';
+
+const app = express();
 
 app.use(cookieParser);
 app.use(queryParser);
 
 app.use('/api', router);
-
-// app.get('*', (req, res) => {
-//   res.write(JSON.stringify(req.parsedCookies, null, 4));
-//   res.write('\n');
-//   res.write(JSON.stringify(req.parsedQuery, null, 4));
-//   res.end();
-// });
 
 export default app;
