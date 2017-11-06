ebad6bb633a8a4d584f9a67f525e4f36d7dd9dd4
Anna_Garmashevska <anna.garmashevska@racingpost.com>
2017-11-06 13:53:29 +0200

    refactor http-servers

--- http-servers/html-server.js
+++ http-servers/html-server.js
@@ -11,19 +11,18 @@ const url = path.resolve(__dirname, './views/index.html');
 
 // readFileSync
 http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
 
     let file = null;
     try {
         file = fs.readFileSync(url, 'utf8');
     } catch (err) {
-        res.statusCode = 404;
+        res.writeHead(404);
         res.end('File not found!');
     }
 
     if (file) {
         const content = file.replace('{message}', 'Hello world!!!');
-        res.statusCode = 200;
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(content);
     }
 }).listen(port1, () => { console.log(`Listening port ${port1}...`); });
@@ -40,12 +39,14 @@ const transformTemplate = new Transform({
 });
 
 http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
     fs.createReadStream(url, 'utf8')
       .on('error', () => {
-            res.statusCode = 404;
-            res.end('File not found');
+          res.writeHead(404);
+          res.end('File not found');
         })
+      .on('end', () => {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+      })
       .pipe(transformTemplate)
       .pipe(res);
 })
--- http-servers/plain-text-server.js
+++ http-servers/plain-text-server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const port = 3000;
 
 http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello World!');
-    res.end();
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
 }).listen(port, () => { console.log(`Listening port ${port}...`); });
