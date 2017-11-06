7d77fb2d67f3403e8acd780e7c55ce70e22a2f6f
Anna Garmashevska <anna.garmashevska@gmail.com>
2017-11-06 23:04:56 +0200

    refactor echo server

--- http-servers/echo-server.js
+++ http-servers/echo-server.js
@@ -3,5 +3,5 @@ const port = 3004;
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(req.url.slice(1));
+    req.pipe(res);
 }).listen(port, () => { console.log(`Listening port ${port}...`); });
