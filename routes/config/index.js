4fe9e83e95f177babea992071965bec5cfde378d
Anna Garmashevska <anna.garmashevska@gmail.com>
2017-11-12 23:21:08 +0200

    refactor router for passport auth

---
+++ routes/config/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'googleAuth' : {
+        'clientID'      : '977454526954-bmmcop04or14im6bq2d2bu1fev3rdgjg.apps.googleusercontent.com',
+        'clientSecret'  : 'MNTmer5G_oebFaCisC7HsP97',
+        'callbackURL'   : 'http://localhost:8080/api-pass/auth/google/callback'
+    },
+    'twitterAuth' : {
+        'consumerKey'       : '5WGZI6m1smLt9cpbgAZfUbcjn',
+        'consumerSecret'    : '1ed78vDzGZAfsOnl1QHmNb4oY887fZQ6s4nOhHmtIpGFOPYbxt',
+        'callbackURL'       : 'http://127.0.0.1:8080/api-pass/auth/twitter/callback'
+    },
+    'facebookAuth' : {
+        'clientID'      : 810393999162470,
+        'clientSecret'  : '66aeec00e368e8607b0c2fdd2ded400a',
+        'callbackURL'   : 'http://127.0.0.1:8080/api-pass/auth/facebook/callback'
+    }
+};
--- routes/routerPassport.js
+++ routes/routerPassport.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import BearerStrategy from 'passport-http-bearer';
 import FacebookStrategy from 'passport-facebook';
 import TwitterStrategy from 'passport-twitter';
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+import config from './config';
 import {
     getProducts,
     addProduct,
@@ -26,11 +28,13 @@ router.use(bodyParser.json());
 router.use(require('express-session')({
     secret: 'keyboard cat',
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: true
 }));
 router.use(passport.initialize());
 router.use(passport.session());
 
+
+// Strategies
 passport.use(new LocalStrategy({
       usernameField: 'login',
       passwordField: 'password',
@@ -48,6 +52,7 @@ passport.use(new LocalStrategy({
 passport.use(new BearerStrategy(
   (token, done) => {
       let currentToken = token;
+      console.log(token);
       let result = tokens.find(({ token }) => currentToken === token );
 
       if (result === undefined) {
@@ -58,76 +63,89 @@ passport.use(new BearerStrategy(
   }
 ));
 
-passport.use(new FacebookStrategy({
-      clientID: 810393999162470,
-      clientSecret: '66aeec00e368e8607b0c2fdd2ded400a',
-      callbackURL: "http://127.0.0.1:8080/api-pass/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile.id);
-      return cb(null, profile);
-  }
+passport.use(new GoogleStrategy({
+        clientID: config.googleAuth.clientID,
+        clientSecret: config.googleAuth.clientSecret,
+        callbackURL: config.googleAuth.callbackURL,
+    },
+    (accessToken, refreshToken, profile, done) => done(null, profile)
 ));
 
 passport.use(new TwitterStrategy({
-      consumerKey: '5WGZI6m1smLt9cpbgAZfUbcjn',
-      consumerSecret: '1ed78vDzGZAfsOnl1QHmNb4oY887fZQ6s4nOhHmtIpGFOPYbxt',
-      callbackURL: 'http://localhost:8080/api-pass/auth/twitter/callback'
+      consumerKey: config.twitterAuth.consumerKey,
+      consumerSecret: config.twitterAuth.consumerSecret,
+      callbackURL: config.twitterAuth.callbackURL
   },
-  function(token, tokenSecret, profile, done) {
-    console.log('callback', profile);
-    console.log('done', done);
-      return done((err) => { console.log(err); }, profile);
-  }
+  (token, tokenSecret, profile, done) => done(null, profile)
 ));
 
-passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:8080/api-pass/auth/google/callback"
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ googleId: profile.id }, function (err, user) {
-            return cb(err, user);
-        });
-    }
+passport.use(new FacebookStrategy({
+      clientID: config.facebookAuth.clientID,
+      clientSecret: config.facebookAuth.clientSecret,
+      callbackURL: config.facebookAuth.callbackURL
+  },
+  (accessToken, refreshToken, profile, done) => done(null, profile)
 ));
 
-passport.serializeUser(function(user, cb) {
-    cb((err) => { console.log(err); }, user);
+passport.serializeUser((user, done) => {
+    done(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
-    cb((err) => { console.log(err); }, obj);
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
 
+// log cookies and query
 router.use((req, res, next) => {
     console.info('Cookies:', JSON.stringify(req.parsedCookies, null, 4));
     console.info('Query params:', JSON.stringify(req.parsedQuery, null, 4));
     next();
 });
 
+// main route with all strategies
+router.get('/auth', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+// routes for local strategy
 router.get('/products', passport.authenticate('bearer', { session: false }), getProducts);
 router.post('/products', passport.authenticate('bearer', { session: false }), addProduct);
 router.get('/products/:id([0-9]+)', passport.authenticate('bearer', { session: false }), getProduct);
 router.get('/products/:id([0-9]+)/reviews', passport.authenticate('bearer', { session: false }), getReviews);
 router.get('/users', passport.authenticate('bearer', { session: false }), getUsers);
 router.post('/auth',  passport.authenticate('local', { session: false }), authorizePassport);
+
+
+// to auth with facebook go http://localhost:8080/api-pass/auth/facebook
 router.get('/auth/facebook', passport.authenticate('facebook'));
+
+// to auth with twitter go http://127.0.0.1:8080/api-pass/auth/twitter
 router.get('/auth/twitter', passport.authenticate('twitter'));
-router.get('/auth/google', passport.authenticate('google'));
 
-router.get('/auth/twitter/callback', passport.authenticate('twitter', { session: false }, { failureRedirect: '/' }), (req, res) => {
-    res.send('ok');
+// to auth with google go http://localhost:8080/api-pass/auth/google
+router.get('/auth/google', passport.authenticate('google', { scope:
+  [ 'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read' ]}));
+
+// redirect in case of success for social strategies auth
+router.get('/auth/success', (req, res) => {
+    res.send(`${req.user.displayName}, you are successfully logged in with ${req.parsedQuery.q.toUpperCase()}!`);
 });
 
-router.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }, { failureRedirect: '/' }), (req, res) => {
-    res.send('facebook ok');
-});
+router.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+        res.redirect('/api-pass/auth/success?q=google');
+    });
 
-router.get('/auth/google/return', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/');
-});
+router.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/api-pass/auth/success?q=twitter');
+  });
+
+router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/' }), (req, res) => {
+      res.redirect('/api-pass/auth/success?q=facebook');
+  });
 
 router.get('*', (req, res) => {
     res.sendStatus(404);
